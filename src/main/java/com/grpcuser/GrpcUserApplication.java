681086eaca7e7386281bf72c3645a42f4a8896b2
Minjae Kim <minjae.kim@mytrum.com>
2022-05-16 22:10:38 +0900

    Initial commit

---
+++ src/main/java/com/grpcuser/GrpcUserApplication.java
@@ -0,0 +1,14 @@
+package com.grpcuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcUserApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GrpcUserApplication.class, args);
+    }
+
+}
